955d58234875b2f809fc8225697d6a388d9f4ab2
chenshenhai <cshenhai@gmail.com>
2020-12-30 22:55:34 +0800

    feat: implement src/doc-engine/github readRepositoryList

--- packages/yuz/__tests__/doc-engine/github.test.ts
+++ packages/yuz/__tests__/doc-engine/github.test.ts
@@ -2,24 +2,45 @@ import path from 'path';
 import fs from 'fs';
 import should from 'should';
 import 'mocha';
-import { clone } from '../../src/doc-engine/github';
+import { cloneRepository, readRepositoryList } from '../../src/doc-engine/github';
 import { makeFullDir, removeFullDir } from './../../src/util/file';
 
 
 describe('src/doc-engine/github', function () {
-  it('clone', function (done) {
+
+  it('cloneRepository', function (done) {
     this.timeout(60000 * 3);
-    const dirPath = path.join(__dirname, '..', '__assets__', 'dist', 'github', 'example-gitbook');
-    if (fs.existsSync(dirPath)) {
-      removeFullDir(dirPath);
+    const localPath = path.join(__dirname, '..', '__assets__', 'dist', 'github', 'example-gitbook');
+    if (fs.existsSync(localPath)) {
+      removeFullDir(localPath);
     }
-    makeFullDir(dirPath);
-    clone({
+    makeFullDir(localPath);
+    cloneRepository({
       name: 'yuzjs',
-      repo: 'example-gitbook',
-      dirPath: dirPath,
-    }).then((res) => {
-      should(fs.existsSync(path.join(dirPath, 'README.md'))).be.deepEqual(true);
+      repository: 'example-gitbook',
+      localPath: localPath,
+    }).then((res: any) => {
+      should(fs.existsSync(path.join(localPath, 'README.md'))).be.deepEqual(true);
+      done();
+    }).catch(done)
+  });
+
+  it('readRepositoryList', function (done) {
+    this.timeout(60000 * 3);
+    const localPath = path.join(__dirname, '..', '__assets__', 'dist', 'github', 'example-gitbook');
+    
+    readRepositoryList({
+      localPath: localPath,
+    }).then((res: any) => {
+      should(res).be.deepEqual([
+        'README.md',
+        'SUMMARY.md',
+        'docs/001.md',
+        'docs/002.md',
+        'docs/101.md',
+        'docs/102.md',
+        'images/yuz-logo.png'
+      ]);
       done();
     }).catch(done)
   });
--- packages/yuz/src/doc-engine/github.ts
+++ packages/yuz/src/doc-engine/github.ts
@@ -1,15 +1,33 @@
 import simpleGit, { SimpleGit } from 'simple-git';
+import compose from 'koa-compose';
 
-
-export async function clone(params: {
+export async function cloneRepository(params: {
   name: string,
-  repo: string,
-  dirPath: string,
+  repository: string,
+  localPath: string,
 }) {
-  const { name, repo, dirPath } = params;
+  const { name, repository, localPath } = params;
   // const url = `git@github.com:${name}/${repo}.git`;
-  const url = `https://github.com/${name}/${repo}.git`;
+  const url = `https://github.com/${name}/${repository}.git`;
   const git: SimpleGit = simpleGit();
-  const result = await git.clone(url, dirPath);
+  const result = await git.clone(url, localPath);
   return result;
 }
+
+export async function readRepositoryList(params: { localPath: string, }): Promise<string[]> {
+  const { localPath } = params;
+  const git: SimpleGit = simpleGit({baseDir: localPath});
+  let result: string = await git.raw('ls-tree', '-r', '--name-only', 'HEAD');
+  const paths = result.replace(/\\r\\n/ig, '\n').split('\n');
+  const list: string[] = [];
+  paths.forEach((i) => {
+    if (typeof i === 'string' && i.length > 0) {
+      list.push(i);
+    }
+  });
+  return list;
+}
+
+// export async function readListTime(list: string[]): Promise<[]> {
+  
+// }
