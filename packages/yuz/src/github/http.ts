483b81f457d1db4167f39fc27c7627b55982000c
chenshenhai <cshenhai@gmail.com>
2021-01-30 00:14:59 +0800

    chore: add core info

--- packages/yuz/src/github/http.ts
+++ packages/yuz/src/github/http.ts
@@ -57,6 +57,7 @@ export async function downloadRepoZip(
 // https://api.github.com/repos/yuzjs/example-gitbook/commits
 // https://api.github.com/repos/yuzjs/example-gitbook/compare/yuzjs:2f80920...yuzjs:e98545b
 // https://api.github.com/repos/yuzjs/example-gitbook/commits/e98545b466cca52b53915fd0eb0bbea39ebeda2d
+// https://api.github.com/repos/yuzjs/example-gitbook/contents/SUMMARY.md
 // https://api.github.com/repos/yuzjs/example-gitbook/contents/
 // 30425b7431173a91c7f78592296a850f7fd3da6b
 // c5a82a1881f8275071dbe5c0dcf14adddc5dd90b
